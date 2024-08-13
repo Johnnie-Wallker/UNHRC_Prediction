5841ac60f537fafd94c0bc0192e69c3b24603d23
Johnnie-Wallker <chen1010954957@gmail.com>
2024-08-13 15:04:01 +0800

    增加记录Summary功能来避免多次调用

--- Prompt_Generator.py
+++ Prompt_Generator.py
@@ -1,15 +1,50 @@
-import warnings
+import os
 import pandas as pd
 from openai import OpenAI
 from CandidateInformation import candidate_information
 from RuleSet import ruleset_generator
 from Prototype import prototype_generator
-
-warnings.simplefilter(action='ignore', category=FutureWarning)
 pd.options.mode.copy_on_write = True
 
 
-def prompt_generator(data, education, work, task_id, prompt_type):
+def summary_writer(information, mandate, task_id):
+    folder_path = os.path.join('Summary', task_id)
+    os.makedirs(folder_path, exist_ok=True)
+    file_path = os.path.join(folder_path, "summary.txt")
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            summary = file.read()
+    else:
+        client = OpenAI(api_key="sk-a5ed383c9510411fa288cf6d2bd8b52d", base_url="https://api.deepseek.com")
+        response = client.chat.completions.create(
+            model="deepseek-chat",
+            messages=[
+                {"role": "user", "content": f'You are an academic that is now studying what qualities a successful '
+                                            f'candidate for {mandate} in the United Nations Human Rights Council.'
+                                            f'The candidates information in these UNHRC meetings are {information}.'
+                                            f'Summarise the keys features of successful candidates, \n'
+                                            f'focus (but not only focus) on the following points:\n'
+                                            f'1. Is there a trend in age or language abilities '
+                                            f'among the successful candidates?'
+                                            f'2. Does certain gender has an advantage over the other?\n'
+                                            f'3. Does certain nationality has an advantage over the others?\n'
+                                            f'4. Does the legal tradition (Based on nationality, '
+                                            f'location of education etc.) of the candidate make '
+                                            f'the candidate more likely to be successful?\n'
+                                            f'5. Does the location of education or the diversity of the location '
+                                            f'of education(i.e. whether the university is in a OECD country or in '
+                                            f'the global south) makes the candidate more likely to be successful?'},
+            ],
+            stream=False
+        )
+        summary = response.choices[0].message.content
+        with open(file_path, 'w') as file:
+            file.write(summary)
+
+    return summary
+
+
+def prompt_generator(data, education, work, task_id, prompt_type, detail=True):
     description = ''
     # 填充空缺值
     data = data.fillna(0)
@@ -42,7 +77,7 @@ def prompt_generator(data, education, work, task_id, prompt_type):
             edu_data_new = education[education['id'].isin(task_data_new['id'])]
             work_data_new = work[work['id'].isin(task_data_new['id'])]
             shortlisted_ids = task_data_new[task_data_new['interviewed'] == 1]['id'].tolist()
-            information += (f"{candidate_information(task_data_new, edu_data_new, work_data_new)}"
+            information += (f"{candidate_information(task_data_new, edu_data_new, work_data_new, detail)}"
                             f"The candidates that were shortlisted in this meeting are: "
                             f"{', '.join(map(str, shortlisted_ids))}.\n")
         if prompt_type == 'Train':
@@ -53,46 +88,20 @@ def prompt_generator(data, education, work, task_id, prompt_type):
                 f'In the previous years, the council has held meetings on the same mandate, '
                 f'the information of the candidates in these meetings are:\n{information}'
                 f'Now that you have had some experience, review the candidates curriculum information below, '
-                f'summarise the strengths and characteristics of each '
-                f'candidate, then select EXACTLY {row["count"]} candidates that are to be shortlisted for interview.'
+                f'summarise the strengths and characteristics of each candidate, '
+                f'then select EXACTLY {row["count"]} candidates that are to be shortlisted for interview.'
                 f'Please respond with the following format: '
-                f'@@@ Candidates selected: id1, ..., id{row["count"]} @@@，\n'
+                f'@@@ Candidates selected: id1, ..., id{row["count"]} @@@, '
+                f'do not include your reasons for selecting them，\n'
                 f'IMPORTANT: Before selecting any candidate, make sure you have thoroughly reviewed the entire '
-                f'list of ALL the candidates below regardless of the order they appear.\n'
+                f'list of ALL the candidates below regardless of the order they appear and make sure you do not select'
+                f'any candidate from any previous meetings, only consider those in the information below.\n'
                 f'The candidates information are:\n'
             )
-            description += candidate_information(task_data, edu_data, work_data) + "\n!!!"
-            description += (f'IMPORTANT: Make sure you do not select any candidate from any previous meetings, '
-                            f'they are only there for reference, you should only select those candidates '
-                            f'under "The candidates information are:".')
+            description += candidate_information(task_data, edu_data, work_data, detail) + "\n!!!"
         if prompt_type == 'Summary':
             row = task_data.iloc[0]
-            client = OpenAI(api_key="sk-a5ed383c9510411fa288cf6d2bd8b52d", base_url="https://api.deepseek.com")
-            response = client.chat.completions.create(
-                model="deepseek-chat",
-                temperature=1,
-                messages=[
-                    {"role": "user", "content": f'You are an academic that is now studying what qualities a successful '
-                                                f'candidate possess for {row["mandate"]} '
-                                                f'in the United Nations Human Rights Council(UNHRC).'
-                                                f'The candidates information in these UNHRC meetings are {information}.'
-                                                f'Based on this information, summarise the keys features '
-                                                f'of a successful candidate.\n'
-                                                f'Focus (but not only focus) on the following points:\n'
-                                                f'1. Is there a trend in age or language abilities '
-                                                f'among the successful candidates?'
-                                                f'2. Does certain gender has an advantage over the other?\n'
-                                                f'3. Does certain nationality has an advantage over the others?\n'
-                                                f'4. Does the legal tradition (Based on nationality, '
-                                                f'location of education etc.) of the candidate make '
-                                                f'the candidate more likely to be successful?\n'
-                                                f'5. Does the location of education or the diversity of the location '
-                                                f'of education(i.e. whether the university is in a OECD country or in '
-                                                f'the global south) makes a candidate more likely to be successful?'},
-                ],
-                stream=False
-            )
-            summary = response.choices[0].message.content
+            summary = summary_writer(information, row['mandate'], task_id)
             description = (
                 f'You are a member of the United Nations Human Rights Council(UNHRC), '
                 f'the council is now holding a meeting for selecting {row["mandate"]}.\n'
@@ -107,7 +116,7 @@ def prompt_generator(data, education, work, task_id, prompt_type):
                 f'list of ALL the candidates below regardless of the order they appear.\n'
                 f'The candidates information are:\n'
             )
-            description += candidate_information(task_data, edu_data, work_data) + "\n"
+            description += candidate_information(task_data, edu_data, work_data, detail) + "\n"
         if prompt_type == 'RuleSet':
             ruleset = ruleset_generator(train_data)
             row = task_data.iloc[0]
@@ -125,7 +134,7 @@ def prompt_generator(data, education, work, task_id, prompt_type):
                 f'list of ALL the candidates below regardless of the order they appear.\n'
                 f'The candidates information are:\n'
             )
-            description += candidate_information(task_data, edu_data, work_data) + "\n"
+            description += candidate_information(task_data, edu_data, work_data, detail) + "\n"
         if prompt_type == 'Prototype':
             prototype = prototype_generator(train_data)
             row = task_data.iloc[0]
@@ -143,7 +152,7 @@ def prompt_generator(data, education, work, task_id, prompt_type):
                 f'list of ALL the candidates below regardless of the order they appear.\n'
                 f'The candidates information are:\n'
             )
-            description += candidate_information(task_data, edu_data, work_data) + "\n"
+            description += candidate_information(task_data, edu_data, work_data, detail) + "\n"
     else:
         row = task_data.iloc[0]
         description = (
@@ -152,11 +161,12 @@ def prompt_generator(data, education, work, task_id, prompt_type):
             f'Review the candidates curriculum information below, summarise the strengths and characteristics of each '
             f'candidate, then select EXACTLY {row["count"]} candidates that are to be shortlisted for interview.'
             f'Please respond with the following format: '
-            f'@@@ Candidates selected: id1, ..., id{row["count"]} @@@，\n'
+            f'@@@ Candidates selected: id1, ..., id{row["count"]} @@@, '
+            f'do not include your reasons for selecting them，\n'
             f'IMPORTANT: Before selecting any candidate, make sure you have thoroughly reviewed the entire '
             f'list of ALL the candidates below regardless of the order they appear.\n'
             f'The candidates information are:\n'
         )
-        description += candidate_information(task_data, edu_data, work_data) + "\n"
+        description += candidate_information(task_data, edu_data, work_data, detail) + "\n"
 
     return description
