29ae8d04252eb528742f20b1caf9eecf0efccf77
Johnnie-Wallker <chen1010954957@gmail.com>
2024-08-20 12:18:09 +0800

    使用此文件运行DeepSeek

--- run_llm.py
+++ run_llm.py
@@ -2,8 +2,8 @@ from DeepSeek import run_deepseek
 from openai import OpenAI
 
 config = {
-    'prompt_type': 'Train',
-    'stage': 2,
+    'prompt_type': 'Summary',
+    'stage':1,
     'shuffle': False,
     'detail': True,
     'vote_count': 1,
@@ -11,11 +11,5 @@ config = {
     'model': "deepseek-chat"
 }
 
-results = run_deepseek(config)
+results = run_deepseek(config, save_result=True)
 print(f'准确率为：{round(results["accuracy"], 3)} 召回率为：{round(results["f1_score"], 3)}')
-
-# 简历筛选轮：
-# 无样例（10次投票） 准确率为：0.685 召回率为：0.524
-# 有样例（10次投票） 准确率为：0.683 召回率为：0.519
-# 总结文本（10次投票） 准确率为：0.696 召回率为：0.541
-# 无样例（20次投票） 准确率为：0.704 召回率为：0.552
