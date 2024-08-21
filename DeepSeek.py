7ff7f57c13938000e050b2222cfc32c14ff717e6
Johnnie-Wallker <chen1010954957@gmail.com>
2024-08-21 10:37:24 +0800

    现在run_llm支持运行small_group方法，同时为small_group添加多轮投票功能

--- DeepSeek.py
+++ DeepSeek.py
@@ -7,18 +7,10 @@ from Result_Logger import log_result
 from collections import Counter
 
 
-def run_deepseek(config, save_result=False):
-    # 参数读取
-    prompt_type = config['prompt_type']
-    stage = config['stage']
-    shuffle = config['shuffle']
-    detail = config['detail']
-    vote_count = config['vote_count']
-    client = config['client']
-    model = config['model']
+def run_deepseek(save_result=False, **kwargs):
     # 读取数据
     data = pd.read_excel('data.xlsx')
-    data = data_handler(data, stage)
+    data = data_handler(data, kwargs['stage'])
     education = pd.read_excel('data.xlsx', sheet_name=1)
     work = pd.read_excel('data.xlsx', sheet_name=2)
     # 填充空缺值
@@ -39,30 +31,82 @@ def run_deepseek(config, save_result=False):
     # 遍历每个任务ID
     for i in range(len(data['task_id'].unique())):
         task_id = data['task_id'].unique()[i]
-        numbers = []
+        task_df = data[data['task_id'] == task_id]
+        count = task_df['count'].max()
+        group = task_df.sample(count + 1)
+        task_df = task_df[~task_df['id'].isin(group['id'])]
         response = None
-        count = data[data['task_id'] == task_id]['count'].max()
-        for j in range(vote_count):
+        numbers = []
+        for j in range(kwargs['vote_count']):
             retries = 0
-            task_description = prompt_generator(data, education, work, task_id, prompt_type, stage=stage,
-                                                shuffle=shuffle, model=model, client=client, detail=detail)
-            while retries < 5:
-                response = client.chat.completions.create(
-                    model=model,
-                    messages=[
-                        {"role": "user", "content": f'{task_description}'},
-                    ],
-                    stream=False
-                )
-                match = re.search(r'@@@ Candidates selected: (.*?) @@@', response.choices[0].message.content)
-                if match:
-                    pred_numbers = re.findall(r'\b[a-f0-9]{6}\b', match.group(1))
-                    if len(pred_numbers) == count:
-                        numbers.append(pred_numbers)
-                        break
-                else:
-                    retries += 1
-                    print('Error occurred, retrying...')
+            if not kwargs['do_small_group'] or len(task_df) == 0:
+                task_description = prompt_generator(data=data,
+                                                    education=education,
+                                                    work=work,
+                                                    task_id=task_id,
+                                                    prompt_type=kwargs['prompt_type'],
+                                                    stage=kwargs['stage'],
+                                                    shuffle=kwargs['shuffle'],
+                                                    model=kwargs['model'],
+                                                    client=kwargs['client'],
+                                                    detail=kwargs['detail'])
+                while retries < kwargs['n_retry']:
+                    response = kwargs['client'].chat.completions.create(
+                        model=kwargs['model'],
+                        messages=[
+                            {"role": "user", "content": f'{task_description}'},
+                        ],
+                        stream=False
+                    )
+                    match = re.search(r'@@@ Candidates selected: (.*?) @@@', response.choices[0].message.content)
+                    if match:
+                        pred_numbers = re.findall(r'\b[a-f0-9]{6}\b', match.group(1))
+                        if len(pred_numbers) == count:
+                            numbers.append(pred_numbers)
+                            break
+                    else:
+                        retries += 1
+                        print(f'The response given is: {response.choices[0].message.content}')
+                        print('Error occurred, retrying...')
+            else:
+                numbers_temp = None
+                while len(task_df) != 0:
+                    task_description = prompt_generator(data=data,
+                                                        education=education,
+                                                        work=work,
+                                                        task_id=task_id,
+                                                        prompt_type=kwargs['prompt_type'],
+                                                        stage=kwargs['stage'],
+                                                        shuffle=kwargs['shuffle'],
+                                                        model=kwargs['model'],
+                                                        client=kwargs['client'],
+                                                        detail=kwargs['detail'],
+                                                        small_group=group)
+                    while retries < kwargs['n_retry']:
+                        response = kwargs['client'].chat.completions.create(
+                            model=kwargs['model'],
+                            messages=[
+                                {"role": "user", "content": f'{task_description}'},
+                            ],
+                            stream=False
+                        )
+                        match = re.search(r'@@@ Candidates selected: (.*?) @@@',
+                                          response.choices[0].message.content)
+                        if match:
+                            numbers_temp = re.findall(r'\b[a-f0-9]{6}\b', match.group(1))
+                            if len(numbers_temp) == count:
+                                numbers.append(numbers_temp)
+                                break
+                        else:
+                            retries += 1
+                            print(f'The response given is: {response.choices[0].message.content}')
+                            print('Error occurred, retrying...')
+                    group = group[group['id'].isin(numbers_temp)]
+                    new_data = task_df.sample(1)
+                    group = pd.concat([group, new_data])
+                    group = group.sample(frac=1).reset_index(drop=True)
+                    task_df = task_df[~task_df['id'].isin(new_data['id'])]
+                numbers.append(numbers_temp)
         all_md5_hashes = [md5 for sublist in numbers for md5 in sublist]
         numbers = [md5 for md5, _ in Counter(all_md5_hashes).most_common(count)]
         token_count.append(response.usage.prompt_tokens)
@@ -80,6 +124,9 @@ def run_deepseek(config, save_result=False):
     acc = accuracy_score(data['interviewed'], data['pred'])
     f1 = f1_score(data['interviewed'], data['pred'])
     if save_result:
-        log_result(data, stage, 'DeepSeek', f'{prompt_type}', token_count)
+        if not kwargs['do_small_group']:
+            log_result(data, kwargs['stage'], 'DeepSeek', {kwargs['prompt_type']}, token_count)
+        else:
+            log_result(data, kwargs['stage'], 'DeepSeek', f"{kwargs['prompt_type']}_SmallGroup", token_count)
 
     return {'accuracy': acc, 'f1_score': f1}
--- run_llm.py
+++ run_llm.py
@@ -2,14 +2,16 @@ from DeepSeek import run_deepseek
 from openai import OpenAI
 
 config = {
-    'prompt_type': 'Summary',
+    'prompt_type': 'None',
     'stage':1,
     'shuffle': False,
-    'detail': True,
+    'detail': False,
     'vote_count': 1,
+    'n_retry': 5,
+    'do_small_group': True,
     'client': OpenAI(api_key="sk-a5ed383c9510411fa288cf6d2bd8b52d", base_url="https://api.deepseek.com"),
     'model': "deepseek-chat"
 }
 
-results = run_deepseek(config, save_result=True)
+results = run_deepseek(**config, save_result=False)
 print(f'准确率为：{round(results["accuracy"], 3)} 召回率为：{round(results["f1_score"], 3)}')
