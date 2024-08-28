9b2a6f50192a5296ea65db918fb112a000d05cab
Johnnie-Wallker <chen1010954957@gmail.com>
2024-08-28 10:47:13 +0800

    更新

--- DeepSeek.py
+++ DeepSeek.py
@@ -8,27 +8,20 @@ from collections import Counter
 
 
 def run_deepseek(save_result=False, **kwargs):
-    # 读取数据
     data = pd.read_excel('data.xlsx')
     data = data_handler(data, kwargs['stage'])
     education = pd.read_excel('data.xlsx', sheet_name=1)
     work = pd.read_excel('data.xlsx', sheet_name=2)
-    # 填充空缺值
     data = data.fillna(0)
     data.iloc[:, 5:25] = data.iloc[:, 5:25].astype('int')
-    # 替换年龄空缺值
     data['age'] = data['age'].replace(0, 'Unknown')
-    # 对语言能力进行替换
     replace_dict = {3: 'high', 2: 'intermediate', 1: 'low', 0: 'no'}
     data.iloc[:, 6:12] = data.iloc[:, 6:12].map(replace_dict.get)
-    # 准备记录结果
     id_pred_map = {id_: 0 for id_ in data['id'].unique()}
     token_count = []
-    # 将ID信息转换为MD5码
     data['id'] = data['id'].apply(lambda x: int_md5_transform(num=x))
     education['id'] = education['id'].apply(lambda x: int_md5_transform(num=x))
     work['id'] = work['id'].apply(lambda x: int_md5_transform(num=x))
-    # 遍历每个任务ID
     for i in range(len(data['task_id'].unique())):
         task_id = data['task_id'].unique()[i]
         task_df = data[data['task_id'] == task_id]
@@ -125,11 +118,9 @@ def run_deepseek(save_result=False, **kwargs):
             id_pred_map[number] = 1
         print(f'Current Progress: {round(((i + 1) / len(data["task_id"].unique())) * 100, 1)}%\n '
               f'Task ID: {task_id} Candidates shortlisted are: {numbers}')
-    # 提取结果
     pred = pd.DataFrame(list(id_pred_map.items()), columns=['id', 'pred'])
     data['id'] = data['id'].apply(lambda x: int_md5_transform(md5_hash=x, reverse=True))
     data = pd.merge(data, pred, on='id', how='left')
-    # 评估结果
     acc = accuracy_score(data['interviewed'], data['pred'])
     f1 = f1_score(data['interviewed'], data['pred'])
     if save_result:
